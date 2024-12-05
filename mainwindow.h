32936e6be1185499ce858e33b1c784233a7cc49b
dali-10 <ghrissi.mohamedali@esprit.tn>
2024-12-05 17:30:36 +0100

    first commit mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include <QMainWindow>
+#include"acceuil.h"
+#include "ui_mainwindow.h"
+
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_connexion_clicked();
+
+
+
+private:
+    Ui::MainWindow *ui;
+    acceuil *acc;
+};
+#endif // MAINWINDOW_H
